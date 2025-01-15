66b246ca3931f14cda78d0d027493a670f277fed
wizard supreme <smn7818@gmail.com>
2025-01-15 11:23:24 -0500

    fix: testing vercel deployment

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
 
   // GitHub pages deployment config
   organizationName: 'wizardsupreme', // GitHub org/user name
-  projectName: 'docs', // Repository name
+  projectName: 'brain', // Repository name
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

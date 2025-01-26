89adec6e810c3a1aec8475b558fd27f5ff2df7c7
nayeem.ai <smn7818@gmail.com>
2025-01-26 06:17:20 -0500

    feat: added secret chris yu page (#20)

---
+++ docs/src/pages/chris.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ChrisPage() {
+  return (
+    <>
+      <script src="https://elevenlabs.io/convai-widget/index.js" async type="text/javascript" />
+      <elevenlabs-convai
+        agent-id="YXj59JtNiOUMjTmp98Vz"
+        // @ts-ignore - ElevenLabs widget element
+      />
+    </>
+  );
+}
